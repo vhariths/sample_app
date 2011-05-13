6188ad8f19c162816e2c16191907f252f0385b2f
vhariths <vedu.hariths@nsquaredglobal.com>
2011-05-13 16:49:55 -0400

    Added tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title="Sample application follow on| Home"
   end
 
   def contact
+    @title="Sample application follow on| Contact"
   end
   
   def about
+     @title="Sample application follow on| About"
   end
 
 end
